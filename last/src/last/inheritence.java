ae4264085d8b594e66145b62f3717ece0cd9f07a
Akhilakumaster <102847091+Akhilakumaster@users.noreply.github.com>
2022-04-17 12:24:39 +0530

    INHERITENCE

---
+++ last/src/last/inheritence.java
@@ -0,0 +1,26 @@
+package last;
+
+public class inheritence 
+{
+	void rrit()
+	{
+		System.out.print("RRIT");
+	}
+}	
+class ece extends inheritence
+{
+		public static void main(String[] args)
+		{
+			ece hod=new ece();
+			hod.rrit();
+		}
+		
+		
+
+}
+		
+		
+
+	
+
+
---
+++ last/src/last/str.java
@@ -0,0 +1,16 @@
+package last;
+
+public class str {
+
+	public static void main(String[] args) {
+
+String str = "Programming";
+
+System.out.println(str.length());
+
+String s = "Let's continue";
+
+System.out.println(s.length());
+
+	}
+}

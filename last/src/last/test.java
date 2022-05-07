3da6196bddfc9343a8618dad97e651e378428179
Akhilakumaster <102847091+Akhilakumaster@users.noreply.github.com>
2022-05-07 12:24:17 +0530

    exeption 1

---
+++ last/src/last/test.java
@@ -0,0 +1,25 @@
+package last;
+import java.util.*;
+public class test {
+
+	public static void main(String[] args) {
+		
+		Scanner s =new Scanner(System.in);
+		 int n=s.nextInt();  
+		 for(int i=0; i<=n; i++)
+		 { 
+		 try
+		 { 
+		 int data=100/i; 
+		 }
+		 catch(Exception e)
+		 {
+		 System.out.println("u dont have brain ");
+		 } 
+		  
+		 finally{
+			 System.out.println("get lost ");	 
+		 }
+	}
+	}
+}

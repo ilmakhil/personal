b087b3dfb252a539e31da71738bbf5d0f4b3df6b
Akhilakumaster <102847091+Akhilakumaster@users.noreply.github.com>
2022-05-12 09:50:41 +0530

    smallest sma

---
+++ last/src/last/PRACTM.java
@@ -0,0 +1,22 @@
+package last;
+import java.util.Scanner;
+
+
+public class PRACTM {
+	public static void main(String[] args) 
+	{
+	Scanner s = new Scanner(System.in);
+	System.out.print("enter first nuber");
+	int p=s.nextInt();
+	System.out.print("enter second nuber");
+	int q=s.nextInt();
+	System.out.print("enter thied nuber");
+	int r=s.nextInt();
+	System.out.print(small(p,q,r));
+	}
+	
+static int small(int p,int q,int r)
+{
+	return Math.min(Math.min(q, r),p);
+}
+}
